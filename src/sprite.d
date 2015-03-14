caa53cd171e4a91647a7402806c064541a0a3f6d
Jake Probst <jake.probst@gmail.com>
2015-03-14 15:53:24 -0700

    makesprite doesnt add duplicate components

--- src/sprite.d
+++ src/sprite.d
@@ -9,15 +9,16 @@ void MakeSprite(GameObject o, string path, int x, int y) {
     CTexture tex = o.get!CTexture();
     if (tex is null) {
         tex = new CTexture;
+        o.add(tex);
     }
     tex.texture = _T.loadFile("resources/images/" ~ path);
-    o.add(tex);
+
 
     CPosition pos = o.get!CPosition();
     if (pos is null) {
         pos = new CPosition;
+        o.add(pos);
     }
     pos.x = x;
     pos.y = y;
-    o.add(pos);
 }
