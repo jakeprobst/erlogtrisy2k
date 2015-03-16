9d89063943668a7e02c3b559ae95e87b61974b96
Jake Probst <jake.probst@gmail.com>
2015-03-16 02:40:25 -0700

    init gamemenu.d

---
+++ src/gamemenu.d
@@ -0,0 +1,38 @@
+module erlogtrisy2k.gamemenu;
+
+
+
+import erlogtrisy2k.scene;
+
+
+
+
+
+
+
+class GameMenu : Scene {
+    GameObject cursor;
+
+
+
+
+
+    this () {
+    }
+    ~this() {
+    }
+
+
+
+
+
+    override void initialize() {
+    }
+    override void suspend() {
+    }
+    override void unsuspend() {
+    }
+    override void destroy() {
+    }
+
+}
