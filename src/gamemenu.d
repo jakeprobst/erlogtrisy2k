fffd12dd690c710d718e87788394814af734e394
Jake Probst <jake.probst@gmail.com>
2015-03-20 00:58:33 -0700

    holy fuck flamercockz stop complaining that it doesnt build

--- src/gamemenu.d
+++ src/gamemenu.d
@@ -3,17 +3,19 @@ module erlogtrisy2k.gamemenu;
 
 
 import erlogtrisy2k.scene;
+import erlogtrisy2k.gameobject;
+import erlogtrisy2k.input;
 
+import erlogtrisy2k.titlescreen;
 
-
-
+import std.stdio;
 
 
 
 class GameMenu : Scene {
     GameObject cursor;
-
-
+    //GameObject[] jlptlevels;
+    //GameObject[] tetrislevels;
 
 
 
@@ -24,15 +26,25 @@ class GameMenu : Scene {
 
 
 
-
+    bool goBackToTitle(GameObject o) {
+        engine.popScene();
+        return true;
+    }
 
     override void initialize() {
+        cursor = new GameObject;
+        CInput i = cursor.getAlways!CInput();
+
+        i.action[InputType.KeyboardDown][Button.Escape] = &goBackToTitle;
     }
     override void suspend() {
+        destroy();
     }
     override void unsuspend() {
+        initialize();
     }
     override void destroy() {
+        delete cursor;
     }
 
 }
