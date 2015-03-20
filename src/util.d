b051fb4ae425a468972cfb64f0bc7f07a4fc5999
Jake Probst <jake.probst@gmail.com>
2015-03-20 03:39:18 -0700

    color structure

--- src/util.d
+++ src/util.d
@@ -1,6 +1,25 @@
 module erlogtrisy2k.util;
 
 import std.string;
+import std.conv;
+
+struct Color {
+    ubyte r,g,b;
+    this(int x, int y, int z) {
+        r = cast(ubyte)x;
+        g = cast(ubyte)y;
+        b = cast(ubyte)z;
+    }
+
+    this(ubyte x, ubyte y, ubyte z) {
+        r = x;
+        g = y;
+        b = z;
+    }
+}
+
+
+
 
 static string[] expandString(string s, int b, int e) {
     string[] o;
